49647375fa63093387b614f630c90ce9c6f15d2c
aiirononeko <aiirononeko@gmail.com>
2024-08-25 22:14:36 +0900

    ヘッダのopacity調整

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className='px-5 md:px-8 flex justify-between items-center h-16 border-b bg-opacity-70 fixed border-white w-full'>
+    <header className='px-5 md:px-8 flex justify-between items-center h-16 border-b bg-white bg-opacity-50 fixed border-white w-full'>
       <h1>ReserveEase</h1>
     </header>
   )
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,24 +7,26 @@ export default function Index() {
     <div>
       <div className='h-screen'>
         {/* <img src='/assets/hero.webp' /> */}
-        <div className='bg-[#CFE8E7] h-full flex flex-col justify-center space-y-10 p-5 pt-16 sm:p-10 md:p-32 lg:p-80 xl:p-96'>
-          <div className='font-medium text-5xl space-y-4'>
-            <p>Reserve</p>
-            <p>Ease</p>
+        <div className='bg-[#CFE8E7] h-full flex flex-col justify-center items-center p-5 pt-16'>
+          <div className='space-y-10 lg:space-y-8'>
+            <div className='font-medium text-5xl space-y-4'>
+              <p>Reserve</p>
+              <p>Ease</p>
+            </div>
+            <div className='space-y-2 font-medium'>
+              <p>
+                フリーランスや個人事業主のための
+                <br />
+                シンプルで洗練された予約システム
+              </p>
+              <p>
+                ブランドイメージを損ねない予約ページで、顧客に最高の予約体験を提供します
+              </p>
+            </div>
+            <Button asChild className='w-2/3 max-w-80'>
+              <Link to='/'>初月無料で使ってみる</Link>
+            </Button>
           </div>
-          <div className='space-y-2 font-medium'>
-            <p>
-              フリーランスや個人事業主のための
-              <br />
-              シンプルで洗練された予約システム
-            </p>
-            <p>
-              ブランドイメージを損ねない予約ページで、顧客に最高の予約体験を提供します
-            </p>
-          </div>
-          <Button asChild className='w-2/3 max-w-80'>
-            <Link to='/'>初月無料で使ってみる</Link>
-          </Button>
         </div>
       </div>
     </div>
