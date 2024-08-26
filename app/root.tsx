7dfd3ced1efd5ffd1f25a2fc88da1b16e7b11d64
aiirononeko <aiirononeko@gmail.com>
2024-08-27 01:19:22 +0900

    GAの設定

--- app/root.tsx
+++ app/root.tsx
@@ -1,19 +1,66 @@
+import { LoaderFunctionArgs } from '@remix-run/cloudflare'
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  json,
+  useLoaderData,
+  useLocation,
 } from '@remix-run/react'
+import { useEffect } from 'react'
+import * as gtag from '~/utils/gtags.client'
 
 import { Footer } from './components/footer'
 import { Header } from './components/header'
 import './tailwind.css'
 
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  return json({
+    gaTrackingId:
+      context.cloudflare.env.ENV === 'production'
+        ? context.cloudflare.env.GA_TRACKING_ID
+        : null,
+  })
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
+  const location = useLocation()
+  const { gaTrackingId } = useLoaderData<typeof loader>()
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId)
+    }
+  }, [location, gaTrackingId])
+
   return (
     <html lang='ja'>
       <head>
+        {gaTrackingId && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id='gtag-init'
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
