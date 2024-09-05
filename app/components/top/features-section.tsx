420b0f0438f259fdfa65b8ef7481c55885a5319e
aiirononeko <aiirononeko@gmail.com>
2024-09-06 00:40:10 +0900

    画像差し替え

--- app/components/top/features-section.tsx
+++ app/components/top/features-section.tsx
@@ -1,14 +1,20 @@
-import howToUseImg from '/assets/top-how-to-use.webp'
+import featuresImg from '/assets/top-features.webp'
 
 export const FeaturesSection = () => {
   return (
     <div className='p-5 flex flex-col justify-center items-center'>
       <div className='space-y-10 max-w-[620px]'>
         <h2 className='font-medium text-2xl underline decoration-[#DCEFEE] decoration-4 md:decoration-8'>
-          導入から予約完了までの流れ。
+          予約管理に必要な機能は全て揃っています。
         </h2>
+        <div className='space-y-2'>
+          <p>
+            ネット予約・事前決済・予約管理・顧客管理・カルテ管理・リマインドメール送信
+          </p>
+          <p>すべての機能を初月無料でまずはお試しいただけます。</p>
+        </div>
         <div className='w-full h-52 md:h-80 rounded-lg'>
-          <img src={howToUseImg} />
+          <img src={featuresImg} />
         </div>
       </div>
     </div>
